685b3e8acfb7cedd7e2631d4c3352b005e8c57f7
packo0 <pdimitrov1389@gmail.com>
2018-02-22 01:17:38 +0200

    solve task heists

--- exercise/t08_ArraysAndMethodsMoreExercises/p06_Heists/p06_Heists.cs
+++ exercise/t08_ArraysAndMethodsMoreExercises/p06_Heists/p06_Heists.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Numerics;
 
 namespace p06_Heists
 {
@@ -8,7 +9,52 @@ namespace p06_Heists
         static void Main(string[] args)
         {
             var prices = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int jewel = prices[0];
+            int gold = prices[1];
 
+            string input = Console.ReadLine();
+            string[] command = new string[2];
+            if (input != "Jail Time")
+            {
+                command = input.Split(' ');
+            }
+
+            BigInteger totalExpences = 0;
+            BigInteger totalEarnings = 0;
+            while (command[0] != "Jail Time")
+            {
+                string loot = command[0];
+                for (int i = 0; i < loot.Length; i++)
+                {
+                    if (loot[i] == '%')
+                    {
+                        totalEarnings += jewel;
+                    }
+                    else if (loot[i] == '$')
+                    {
+                        totalEarnings += gold;
+                    }
+                }
+
+                totalExpences += int.Parse(command[1]);
+
+                input = Console.ReadLine();
+                if (input == "Jail Time")
+                {
+                    break;
+                }
+
+                command = input.Split(' ');
+            }
+
+            if (totalEarnings >= totalExpences)
+            {
+                Console.WriteLine($"Heists will continue. Total earnings: {totalEarnings - totalExpences}.");
+            }
+            else
+            {
+                Console.WriteLine($"Have to find another job. Lost: {totalExpences - totalEarnings}.");
+            }
         }
     }
 }
